b5d0e784ba963e55e302875c75331c8847ad5dbc
David Hoyes <dphoyes@gmail.com>
2012-03-28 18:33:07 +0100

    Database-driven page content

--- Symfony/src/Icse/PublicBundle/Controller/DefaultController.php
+++ Symfony/src/Icse/PublicBundle/Controller/DefaultController.php
@@ -7,23 +7,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+  private function getSiteText($name)
+    {
+      $textObject = $this->getDoctrine()
+                    ->getRepository('IcsePublicBundle:SiteText')
+                    ->findOneByName($name);
+
+      return $textObject ? $textObject->getText() : "";
+    }
+
   public function indexAction()
     {
-      return $this->render('IcsePublicBundle:Default:home.html.twig', array());
+      return $this->render('IcsePublicBundle:Default:home.html.twig', array('home_intro' => $this->getSiteText('home_intro')));
     }
 
   public function aboutAction()
     {
-      return $this->render('IcsePublicBundle:Default:about.html.twig', array());
+      return $this->render('IcsePublicBundle:Default:generic_page.html.twig', array('pageId' => 'about',
+                                                                                    'pageTitle' => 'About Us',
+                                                                                    'pageBody' => $this->getSiteText('about')));
     }
 
   public function supportAction()
     {
-      return $this->render('IcsePublicBundle:Default:support.html.twig', array());
+      return $this->render('IcsePublicBundle:Default:generic_page.html.twig', array('pageId' => 'support',
+                                                                                    'pageTitle' => 'Support Us',
+                                                                                    'pageBody' => $this->getSiteText('support')));
     }
 
   public function contactAction()
     {
-      return $this->render('IcsePublicBundle:Default:contact.html.twig', array());
+      return $this->render('IcsePublicBundle:Default:generic_page.html.twig', array('pageId' => 'contact',
+                                                                                    'pageTitle' => 'Contact Us',
+                                                                                    'pageBody' => $this->getSiteText('contact')));
     }
 }
---
+++ Symfony/src/Icse/PublicBundle/Entity/SiteText.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Icse\PublicBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Icse\PublicBundle\Entity\SiteText
+ */
+class SiteText
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var text $text
+     */
+    private $text;
+
+    /**
+     * @var datetime $updated_at
+     */
+    private $updated_at;
+
+    /**
+     * @var integer $updated_by
+     */
+    private $updated_by;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set text
+     *
+     * @param text $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * Get text
+     *
+     * @return text 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param datetime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return datetime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set updated_by
+     *
+     * @param integer $updatedBy
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updated_by = $updatedBy;
+    }
+
+    /**
+     * Get updated_by
+     *
+     * @return integer 
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updated_by;
+    }
+}
