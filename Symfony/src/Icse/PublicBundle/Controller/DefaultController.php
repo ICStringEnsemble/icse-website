f69bb4ecff300bd49ca50e11ff83e0a16e5907d4
David Hoyes <dphoyes@gmail.com>
2012-03-30 18:36:36 +0100

    Join Us form frontend

--- Symfony/src/Icse/PublicBundle/Controller/DefaultController.php
+++ Symfony/src/Icse/PublicBundle/Controller/DefaultController.php
@@ -3,7 +3,36 @@
 namespace Icse\PublicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+use Icse\PublicBundle\Entity\Subscriber;
+
+
+if (!function_exists('ldap_get_mail'))
+  {
+    function ldap_get_mail($login)
+    {
+      if ($login == "js10")
+        {
+          return "john.smith10@imperial.ac.uk";
+        }
+      else
+        {
+          return null;
+        }
+    }
+
+    function ldap_get_names($login)
+    {
+      return array("John", "Smith");
+    }
+
+    function ldap_get_info($login)
+    {
+      return array(2 => "Department of X");
+    }
+  }
 
 class DefaultController extends Controller
 {
@@ -26,6 +55,83 @@ class DefaultController extends Controller
       return $this->render('IcsePublicBundle:Default:generic_page.html.twig', array('pageId' => 'about',
                                                                                     'pageTitle' => 'About Us',
                                                                                     'pageBody' => $this->getSiteText('about')));
+    }
+
+  public function joinAction(Request $request)
+    {
+      $subscriber = new Subscriber();
+
+      $form = $this->createFormBuilder()
+              ->add('first_name', 'text')
+              ->add('last_name', 'text')
+              ->add('email', 'email')
+              ->add('login', 'text', array('required' => false))
+              ->add('department', 'text', array('required' => false))
+              ->add('player', 'choice', array(
+                      'choices' => array(
+                        'true' => 'Yes!',
+                        'false' => 'No, just notify me about any concerts.'
+                      ),
+                      'expanded' => true,
+                      'multiple' => false,
+                      'label' => 'Are you interested in joining ICSE?'
+                    ))
+              ->add('instrument', 'choice', array(
+                      'choices' => array(
+                        'Violin' => 'Violin',
+                        'Viola' => 'Viola',
+                        'Cello' => '\'Cello',
+                        'Double Bass' => 'Double Bass',
+                        'other' => 'Other'
+                      ),
+                      'expanded' => true,
+                      'multiple' => false,
+                      'required' => false
+                    ))
+              ->add('other_instrument', 'text', array('label' => ' ', 'required' => false))
+              ->add('standard', 'text', array('label' => 'Give us an idea of your standard', 'required' => false))
+              ->getForm(); 
+
+      if ($request->getMethod() == 'POST')
+        {
+          $form->bindRequest($request);
+          $data = $form->getData();
+          return $this->render('IcsePublicBundle:Default:join.html.twig', array('join_intro' => $this->getSiteText('join_intro')));
+        }
+      else
+        {
+          return $this->render('IcsePublicBundle:Default:join.html.twig', array('join_intro' => $this->getSiteText('join_intro'),
+                                                                                'form' => $form->createView()));
+        }
+    }
+
+  public function query_usernameAction(Request $request)
+    {
+      $input = $request->query->get('input');
+      $output = array();
+      if (filter_var($input, FILTER_VALIDATE_EMAIL))
+        {
+          $output['type'] = 'email';
+        }
+      else
+        {
+          $email_from_username = ldap_get_mail($input);
+          if ($email_from_username)
+            {
+              $output['type'] = 'username';
+              $output['email'] = $email_from_username;
+              $names = ldap_get_names($input);
+              $output['first_name'] = $names[0];
+              $output['last_name'] = $names[count($names)-1];
+              $info = ldap_get_info($input);
+              $output['department'] = $info[2];
+            }
+          else
+            {
+              $output['type'] = 'invalid';
+            }
+        }
+      return new Response(json_encode($output));
     }
 
   public function supportAction()
---
+++ Symfony/src/Icse/PublicBundle/Entity/Subscriber.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Icse\PublicBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Icse\PublicBundle\Entity\Subscriber
+ */
+class Subscriber
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $username
+     */
+    private $username;
+
+    /**
+     * @var string $email
+     */
+    private $email;
+
+    /**
+     * @var string $first_name
+     */
+    private $first_name;
+
+    /**
+     * @var string $last_name
+     */
+    private $last_name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set first_name
+     *
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->first_name = $firstName;
+    }
+
+    /**
+     * Get first_name
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * Set last_name
+     *
+     * @param string $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->last_name = $lastName;
+    }
+
+    /**
+     * Get last_name
+     *
+     * @return string 
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+}
