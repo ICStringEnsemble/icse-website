749bc81a3c25a57358a5f7ce24a8f730e36b6622
David Hoyes <dphoyes@gmail.com>
2013-01-16 01:22:35 +0000

    Improved members nav column, changed migrate DB to AJAX, fixed image zoom origin

--- Symfony/src/Icse/MembersBundle/Controller/SuperAdminController.php
+++ Symfony/src/Icse/MembersBundle/Controller/SuperAdminController.php
@@ -3,23 +3,33 @@
 namespace Icse\MembersBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response; 
 
 
 class SuperAdminController extends Controller
 {
-  public function migrateDBAction()
+    public function membersListAction()
     {
-      exec('/usr/bin/php ./Symfony/app/console -n doctrine:migrations:migrate', $output, $error);
-      if ($error == 0)
+        return $this->render('IcseMembersBundle:SuperAdmin:memberslist.html.twig');
+    }
+
+    public function siteDevAction()
+    {
+        return $this->render('IcseMembersBundle:SuperAdmin:sitedev.html.twig');
+    }
+
+    public function migrateDBAction()
+    {
+        exec('/usr/bin/php ./Symfony/app/console -n doctrine:migrations:migrate', $output, $error);
+        if ($error == 0)
         {
           array_push($output, "Success");
         }
-      else
+        else
         {
           array_push($output, "Fail");
         }
-      $pageBody = "<h1>Database Migration</h1>" . implode('<br />', $output);
-      return $this->render('IcseMembersBundle:Default:generic_page.html.twig', array('pageTitle' => 'Migrate Database',
-                                                                                     'pageBody' => $pageBody));
+        $pageBody = implode('<br />', $output);
+        return new Response($pageBody);
     }
 }
