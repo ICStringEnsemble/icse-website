d2cae34287098f9c50973c62f0fb709555f3e4e9
David Hoyes <dphoyes@gmail.com>
2013-10-16 14:55:19 +0100

    Added new-email editor

--- Symfony/src/Icse/MembersBundle/Controller/Admin/EmailController.php
+++ Symfony/src/Icse/MembersBundle/Controller/Admin/EmailController.php
@@ -8,12 +8,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException; 
 use Icse\MembersBundle\Form\Type\FileInfoType;
 
+use RobertoTru\ToInlineStyleEmailBundle\Converter\ToInlineStyleEmailConverter; 
 
 class EmailController extends Controller
 {
-    public function mainAction()
+    public function mainAction($arg)
     {
-        return $this->render('IcseMembersBundle:Admin:email.html.twig', []);
+        if ($arg == 'preview')
+        {
+
+            $style_converter = $this->get('css_to_inline_email_converter');
+
+            $style_converter->setCSS(file_get_contents($this->container->getParameter('kernel.root_dir') . '/../web/bundles/icsemembers/css/email.css')); 
+
+            $style_converter->setHTMLByView('IcseMembersBundle:Email:dummy.html.twig'); 
+            $html_body = $style_converter->generateStyledHTML();
+            
+            return new Response($html_body);
+        }
+
+
+
+        return $this->render('IcseMembersBundle:Admin:email.html.twig');
 
     }
 }
