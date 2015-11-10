1da10bb78b5fd62b3c249091829af329553462d0
David Hoyes <dphoyes@gmail.com>
2015-11-10 23:15:44 +0000

    Update the login form to be more informative in the event of a login failure (eg. because of membership expiry)

--- Symfony/src/Icse/PublicBundle/Controller/SecurityController.php
+++ Symfony/src/Icse/PublicBundle/Controller/SecurityController.php
@@ -4,6 +4,10 @@ namespace Icse\PublicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use Symfony\Component\Security\Core\Exception\LockedException;
+
 
 class SecurityController extends Controller
 {
@@ -15,9 +19,18 @@ class SecurityController extends Controller
 
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        return $this->render('IcsePublicBundle:Security:login.html.twig', array(
-          'last_username' => $authenticationUtils->getLastUsername(),
-          'error'         => $authenticationUtils->getLastAuthenticationError(),
-        ));
+        $username = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $creds_error   = ($error instanceof BadCredentialsException);
+        $locked_error  = ($error instanceof LockedException);
+        $expired_error = ($error instanceof AccountExpiredException);
+
+        return $this->render('IcsePublicBundle:Security:login.html.twig', [
+          'last_username' => $username,
+          'creds_error'   => $creds_error,
+          'locked_error'  => $locked_error,
+          'expired_error' => $expired_error,
+        ]);
     }
 }
